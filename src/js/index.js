55f9807bd625a73a12fb8d703c6602d78e7aaa39
Margaret <match14@yandex.ru>
2021-05-08 13:53:11 +0300

    top-block

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import {$, Slider} from './common';
 
+var widthWindow = $(window).width();
+
 // Arrow top page
 $(window).on('scroll', function(){
 	if($(this).scrollTop()>300){
@@ -10,85 +12,85 @@ $(window).on('scroll', function(){
 });
 $('.js-move-up').on('click', function(){$('body,html').animate({scrollTop:0},500);return false;});
 
-// //range slider
-// if($('.js-range-input').length){
-// 	$('.js-range-input').each(function(){
-// 		var $parent = $(this).parents('.js-range');
-// 		var $rangeVal = $parent.find('.js-range-val');
-// 		var numMin = $(this).data('slider-min').toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-// 		var numMax = $(this).data('slider-max').toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+//range slider
+if($('.js-range-input').length){
+	$('.js-range-input').each(function(){
+		var $parent = $(this).parents('.js-range');
+		var $rangeVal = $parent.find('.js-range-val');
+		var numMin = $(this).data('slider-min').toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+		var numMax = $(this).data('slider-max').toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 
-// 		if($rangeVal.data('text') == 'день'){
-// 			$rangeVal.html('<span class="range__val-num">' + $(this).data('slider-value') + '</span> ' + nameDay($(this).data('slider-value')));
+		if($rangeVal.data('text') == 'день'){
+			$rangeVal.html('<span class="range__val-num">' + $(this).data('slider-value') + '</span> ' + nameDay($(this).data('slider-value')));
 
-// 			if($parent.data('note') == true){
-// 				numMin = numMin + ' ' + nameDay($(this).data('slider-min'));
-// 				numMax = numMax + ' ' + nameDay($(this).data('slider-max'));
-// 			}
-// 		}else{
-// 			$rangeVal.html('<span class="range__val-num">' + $(this).data('slider-value') + '</span> ' + $rangeVal.data('text'));
+			if($parent.data('note') == true){
+				numMin = numMin + ' ' + nameDay($(this).data('slider-min'));
+				numMax = numMax + ' ' + nameDay($(this).data('slider-max'));
+			}
+		}else{
+			$rangeVal.html('<span class="range__val-num">' + $(this).data('slider-value') + '</span> ' + $rangeVal.data('text'));
 
-// 			if($parent.data('note') == true){
-// 				numMin = numMin + ' ' + $rangeVal.data('text');
-// 				numMax = numMax + ' ' + $rangeVal.data('text');
-// 			}
-// 		}
+			if($parent.data('note') == true){
+				numMin = numMin + ' ' + $rangeVal.data('text');
+				numMax = numMax + ' ' + $rangeVal.data('text');
+			}
+		}
 
-// 		$parent.find('.js-range-min').text(numMin)
-// 		$parent.find('.js-range-max').text(numMax)
-// 	});
+		$parent.find('.js-range-min').text(numMin)
+		$parent.find('.js-range-max').text(numMax)
+	});
 	
 
-// 	$('.js-range-input').on("slide", function(e) {
-// 		var $rangeVal = $(this).parents('.js-range').find('.js-range-val');
+	$('.js-range-input').on("slide", function(e) {
+		var $rangeVal = $(this).parents('.js-range').find('.js-range-val');
 
-// 		if($rangeVal.data('text') == 'день'){
-// 			$rangeVal.html('<span class="range__val-num">'+ e.value + '</span> ' + nameDay(e.value));
-// 		}else{
-// 			$rangeVal.html('<span class="range__val-num">'+ e.value + '</span> ' + $rangeVal.data('text'));
-// 		}
-// 	});
+		if($rangeVal.data('text') == 'день'){
+			$rangeVal.html('<span class="range__val-num">'+ e.value + '</span> ' + nameDay(e.value));
+		}else{
+			$rangeVal.html('<span class="range__val-num">'+ e.value + '</span> ' + $rangeVal.data('text'));
+		}
+	});
 
-// 	function nameDay(val) {
-// 		var lastFigure = parseInt(val.toString().substr(val.toString().length - 1, 1));
+	function nameDay(val) {
+		var lastFigure = parseInt(val.toString().substr(val.toString().length - 1, 1));
 
-// 		if (val >= 11 && val < 15) {
-// 			return 'дней';
-// 		}
-// 		else {
-// 			if (lastFigure == 1) return 'день';
-// 			if (lastFigure > 1 && lastFigure < 5) return 'дня';
-// 			if (lastFigure == 0 || lastFigure >= 5) return 'дней';
-// 		}
-// 	}
-// }
+		if (val >= 11 && val < 15) {
+			return 'дней';
+		}
+		else {
+			if (lastFigure == 1) return 'день';
+			if (lastFigure > 1 && lastFigure < 5) return 'дня';
+			if (lastFigure == 0 || lastFigure >= 5) return 'дней';
+		}
+	}
+}
 
-// // Yandex карта
-// if ($('#map').length) {
-// 	// Иницилизация карты
-// 	ymaps.ready(init);
+// Yandex карта
+if ($('#map').length) {
+	// Иницилизация карты
+	ymaps.ready(init);
 
-// 	function init(){
-// 		var myMap;
+	function init(){
+		var myMap;
 
-// 		myMap = new ymaps.Map("map", {
-// 			center: [64.552, 39.758],
-// 			zoom: 17,
-// 			controls: []
-// 		});
+		myMap = new ymaps.Map("map", {
+			center: [64.552, 39.758],
+			zoom: 17,
+			controls: []
+		});
 
-// 		var myPlacemark = new ymaps.Placemark([64.55215, 39.757584] , {
-// 			balloonContentBody: 'ул. Трухинова, д. 6А',
-// 		},
-// 			{ iconLayout: 'default#image',
-// 			iconImageHref: '/assets/img/mark-map.png',
-// 			iconImageSize: [76, 115],
-// 			iconImageOffset: [-38, -57],
-// 		});
+		var myPlacemark = new ymaps.Placemark([64.55215, 39.757584] , {
+			balloonContentBody: 'ул. Трухинова, д. 6А',
+		},
+			{ iconLayout: 'default#image',
+			iconImageHref: '/assets/img/mark-map.png',
+			iconImageSize: [76, 115],
+			iconImageOffset: [-38, -57],
+		});
 
-// 			myMap.geoObjects.add(myPlacemark);
-// 	}
-// }
+			myMap.geoObjects.add(myPlacemark);
+	}
+}
 
 // // review slider
 // if($('.js-review-slider').length){
